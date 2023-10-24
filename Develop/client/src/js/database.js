a34ff91d212579304708d61d022494e0cc1b33d7
pineonline10 <joshisstillpine@gmail.com>
2023-10-23 22:15:07 -0400

    adds basic db and clones starter code

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,37 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put({ id: 1, content: content });
+  await tx.done;
+  console.log('Data saved to IndexedDB');
+};
+ console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const data = await store.get(1);
+  await tx.done;
+  return data ? data.content : null;
+}; 
+console.error('getDb not implemented');
+
+initdb();
